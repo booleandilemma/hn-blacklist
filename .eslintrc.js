e8f199db1101a74be5ebb9536da5c9c56850166a
booleandilemma <booleandilemma@gmail.com>
2024-12-21 20:32:27 -0500

    Add some eslint exceptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,16 +4,19 @@ module.exports = {
     commonjs: true,
     es2021: true,
   },
-  extends: [ 'airbnb-base', 'eslint:recommended'],
+  extends: ['airbnb-base', 'eslint:recommended'],
   overrides: [
-  {
-    files: [ '*.js' ],
-    rules: {
-      'class-methods-use-this': 'off',
-      'no-plusplus': 'off',
-      'no-console': 'off',
+    {
+      files: ['*.js'],
+      rules: {
+        'class-methods-use-this': 'off',
+        'no-plusplus': 'off',
+        'no-console': 'off',
+        'linebreak-style': 'off',
+        'quotes': 'off',
+        'camelcase': 'off',
+      }
     }
-  }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
